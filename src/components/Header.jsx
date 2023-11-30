3a5c39326c43aa5c0fcd9e4f0040e655869aeee9
Adiletbaike <adiletdzhuraev@gmail.com>
2023-11-30 07:48:42 +0600

    Website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-dom";
 
 const Header = () => {
   return (
-    <header className="text-gray-600 body-font absolute bg-transparent">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+    <header className="text-gray-600 body-font bg-transparent">
+      <div className="container mx-auto flex flex-wrap justify-center p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium  items-center text-gray-900 mb-4 md:mb-0">
           <img
             src="https://iparbio.com/public/images/icon/iparLogo.png"
             alt="LOGO"
-            className="w-40 md:ml-10 invisible md:visible"
+            className="w-40 md:h-0 md:ml-10 visible md:hidden"
           />
         </a>
       </div>
